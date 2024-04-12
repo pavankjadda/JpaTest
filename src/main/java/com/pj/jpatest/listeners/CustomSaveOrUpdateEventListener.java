0e4adfd3b0bd6ee62c8f7d65e61330c1782ad9d7
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-04-11 22:55:41 -0400

    docs: updates application.yml

--- src/main/java/com/pj/jpatest/listeners/CustomSaveOrUpdateEventListener.java
+++ src/main/java/com/pj/jpatest/listeners/CustomSaveOrUpdateEventListener.java
@@ -6,14 +6,12 @@ import org.hibernate.persister.entity.EntityPersister;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomSaveOrUpdateEventListener
-        implements PostDeleteEventListener, PostInsertEventListener, PostUpdateEventListener {
+public class CustomSaveOrUpdateEventListener implements PostDeleteEventListener, PostInsertEventListener, PostUpdateEventListener {
     private final EntityManager entityManager;
 
     public CustomSaveOrUpdateEventListener(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    
 
     @Override
     public void onPostDelete(PostDeleteEvent event) {
